a3bbdcf7594c3b9085d3c4474e907c48f78d56e6
Seba Mamby <s.mamby@gmail.com>
2024-10-30 16:24:22 -0300

    mejorado carrusel de nuestro producto mas exacto y parecido al original, terminado. continuar con tutorials y nuestro local

--- js/nuestrosProductos.js
+++ js/nuestrosProductos.js
@@ -112,6 +112,80 @@ const cards = `
                     </div>
                 </div>
             </div>
+            <!-- Tarjeta 1 -->
+            <div class="card">
+                <div class="card-content">
+                    <div class="card-front">
+                        <img src="img/te1_green_tango.png" alt="Green Tango">
+                        <h3>Green Tango</h3>
+                    </div>
+                    <div class="card-back">
+                        <h3>Green Tango</h3>
+                        <p class="description">
+                            Hebras con aromas frescos y frutales. <br> 
+                            <strong>Ingredientes:</strong> Té verde, yerba y limón.
+                        </p>
+                        <p>Intensidad</p>
+                        <div class="intensity-bar"><span style="width: 60%;"></span></div>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Tarjeta 2 -->
+            <div class="card">
+                <div class="card-content">
+                    <div class="card-front">
+                        <img src="img/te2_masala_chai.png" alt="Masala Chai">
+                        <h3>Masala Chai</h3>
+                    </div>
+                    <div class="card-back">
+                        <h3>Masala Chai</h3>
+                        <p class="description">
+                            Blend con cálidas especias. <br> 
+                            <strong>Ingredientes:</strong> Té negro, pimienta, cardamomo, jengibre.
+                        </p>
+                        <p>Intensidad</p>
+                        <div class="intensity-bar"><span style="width: 80%;"></span></div>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Tarjeta 3 -->
+            <div class="card">
+                <div class="card-content">
+                    <div class="card-front">
+                        <img src="img/te3_cinnamon_rose.png" alt="Cinnamon Rose">
+                        <h3>Cinnamon Rose</h3>
+                    </div>
+                    <div class="card-back">
+                        <h3>Cinnamon Rose</h3>
+                        <p class="description">
+                            Aromático blend frutado. <br> 
+                            <strong>Ingredientes:</strong> Té negro, canela, hibiscus.
+                        </p>
+                        <p>Intensidad</p>
+                        <div class="intensity-bar"><span style="width: 70%;"></span></div>
+                    </div>
+                </div>
+            </div>
+            <!-- Tarjeta 4 -->
+            <div class="card">
+                <div class="card-content">
+                    <div class="card-front">
+                        <img src="img/te4_chocolate.png" alt="Earl Grey">
+                        <h3>Chocolate</h3>
+                    </div>
+                    <div class="card-back">
+                        <h3>Chocolate</h3>
+                        <p class="description">
+                            Clásico con notas cítricas. <br> 
+                            <strong>Ingredientes:</strong> Té negro y bergamota.
+                        </p>
+                        <p>Intensidad</p>
+                        <div class="intensity-bar"><span style="width: 50%;"></span></div>
+                    </div>
+                </div>
+            </div>
 `;
 
 const containerProducts = document.getElementById('nuestprod-carrusel-content');
@@ -126,26 +200,33 @@ const dotBtnNP = Array.from(document.getElementsByClassName('dot-btnNP'));
 
 leftBtnCarrusel.addEventListener('click', function() {
     console.log('pressed left');
-    carruselContentNP.style.justifyContent = 'left';
+    //carruselContentNP.style.justifyContent = 'left';
+    carruselContentNP.style.transition = 'transform 0.5s ease';
+    carruselContentNP.style.transform = 'translateX(0%)'
     dotBtnNP[0].classList.add('active');
     dotBtnNP[1].classList.remove('active');
 })
 rightBtnCarrusel.addEventListener('click', function() {
     console.log('pressed right');
-    carruselContentNP.style.justifyContent = 'right';
+    //carruselContentNP.style.justifyContent = 'right';
+    carruselContentNP.style.transition = 'transform 0.5s ease';
+    carruselContentNP.style.transform = 'translateX(-100%)'
     dotBtnNP[1].classList.add('active');
     dotBtnNP[0].classList.remove('active');
 })
+
 dotBtnNP[0].classList.add('active');
 dotBtnNP[0].addEventListener('click', function() {
-    carruselContentNP.style.justifyContent = 'left';
+    carruselContentNP.style.transition = 'transform 0.5s ease';
+    carruselContentNP.style.transform = 'translateX(0%)'
     dotBtnNP.forEach((e) => {
         e.classList.remove('active');
     })
     dotBtnNP[0].classList.add('active');
 })
 dotBtnNP[1].addEventListener('click', function() {
-    carruselContentNP.style.justifyContent = 'right';
+    carruselContentNP.style.transition = 'transform 0.5s ease';
+    carruselContentNP.style.transform = 'translateX(-100%)'
     dotBtnNP.forEach((e) => {
         e.classList.remove('active');
     })
