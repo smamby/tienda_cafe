1608a5629fddcaa50a9d5358d3f0463201726f4c
Seba Mamby <s.mamby@gmail.com>
2024-10-29 22:17:20 -0300

    [[index]] retoque de estilos en carrusel y script de tres frentes

--- js/scripts.js
+++ js/scripts.js
@@ -3,31 +3,35 @@ const leftBtn = document.getElementsByClassName('left-btn');
 const rightBtn = document.getElementsByClassName('right-btn');
 const backgroundIndex = document.getElementById('background-container');
 dotBtns[0].classList.add('active');
+var i = 0;
 
-dotBtns.forEach((el, i) => {
+dotBtns.forEach((el, ix) => {
     el.addEventListener('click', ()=> {
-        console.log(`boton apretado dot ${i}`);
-        backgroundIndex.style.backgroundImage = `url('./img/home-background-${i}.jpg')`;
+        console.log(`boton apretado dot ${ix}`);
+        backgroundIndex.style.backgroundImage = `url('./img/home-background-${ix}.jpg')`;
+        i = ix;
         dotBtns.forEach((e) => {
-            // e.style.background = 'transparent';
             e.classList.remove('active');
-            // e.style.border = '1px solid white';
         })
-        // el.style.background = 'white';
         el.classList.add('active');
     });
 })
 
-var i = 0;
 leftBtn[0].addEventListener('click', () => {
     console.log(`boton apretado < i= ${i}`)
     if (i == 0) {
         i = 2;
         backgroundIndex.style.backgroundImage = `url('./img/home-background-${i}.jpg')`;
-        return
+        console.log(`foto i= ${i}`)
+    } else {
+        i -= 1;
+        backgroundIndex.style.backgroundImage = `url('./img/home-background-${i}.jpg')`;
+        console.log(`foto i= ${i}`)
     }
-    i -= 1;
-    backgroundIndex.style.backgroundImage = `url('./img/home-background-${i}.jpg')`;
+    dotBtns.forEach((e) => {
+        e.classList.remove('active');
+    })
+    dotBtns[i].classList.add('active');
 })
 
 rightBtn[0].addEventListener('click', () => {
@@ -35,8 +39,30 @@ rightBtn[0].addEventListener('click', () => {
     if (i == 2) {
         i = 0;
         backgroundIndex.style.backgroundImage = `url('./img/home-background-${i}.jpg')`;
-        return
+        console.log(`foto i= ${i}`)
+    } else {
+        i += 1;
+        backgroundIndex.style.backgroundImage = `url('./img/home-background-${i}.jpg')`;
+        console.log(`foto i= ${i}`)
     }
-    i += 1;
-    backgroundIndex.style.backgroundImage = `url('./img/home-background-${i}.jpg')`;
+    dotBtns.forEach((e) => {
+        e.classList.remove('active');
+    })
+    dotBtns[i].classList.add('active');
 })
+
+setInterval(function () {
+    if (i == 2) {
+        i = 0;
+    } else {
+        i += 1;
+    }
+    backgroundIndex.style.backgroundImage = `url('./img/home-background-${i}.jpg')`;
+    console.log(`foto i= ${i}`)
+    
+    dotBtns.forEach((e) => {
+        e.classList.remove('active');
+    });
+    dotBtns[i].classList.add('active');
+        
+}, 5000);
